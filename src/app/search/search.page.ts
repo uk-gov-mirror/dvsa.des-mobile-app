74d7be4180ba92dd7d2e8b3f2d54a8e0b6520212
Pierce Morris <pierce.morris1998@gmail.com>
2020-10-23 16:37:06 +0100

    add data binding and fail search result

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+const searchBy = {
+  DRIVER_NUMBER: 'driverNumber',
+  APP_REF: 'applicationRef'
+};
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
+  searchInput = '4001928472';
+  searchByType = searchBy.DRIVER_NUMBER;
+  searchResults = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSearchByChange(evt) {
+    if (evt.detail.value === searchBy.DRIVER_NUMBER) {
+      this.searchByType = searchBy.DRIVER_NUMBER;
+    } else if (evt.detail.value === searchBy.APP_REF) {
+      this.searchByType = searchBy.APP_REF;
+    }
+  }
+
+  onSearchClick() {
+    console.log(`Searching ${this.searchInput} under ${this.searchByType}`);
+    this.searchResults = [];
+  }
 }
